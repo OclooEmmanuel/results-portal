9e4e03326716fc56a910457d81cb4002559fe5f0
Ocloo Emmanuel <oclooemmanuel02@gmail.com>
2026-02-05 09:34:08 +0000

    Change ALLOWED_HOSTS to allow all hosts
    Updated ALLOWED_HOSTS to allow all hosts.

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "results-port.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
