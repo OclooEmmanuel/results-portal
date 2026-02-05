f0022d46f9282d02e13f3f053242eee5f9517a32
ocloo <oclooemmanuel02@gmail.com>
2026-02-05 08:43:23 +0000

    changed db to postgres

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-pcx&k2m#rqp6j+uoyvz^1(+#ouq%j4yv0aoq$i-b18_6*$=$_0'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'replace-me')
+SECRET_KEY = "django-insecure-yfe)2!@(mu1c6_xs&nl3k1uzd#6job)tj%sjhfxd0$!mh*!"
 # ...existing code...
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -76,21 +76,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASE_URL = os.environ.get("DATABASE_URL")
+
+# if not DATABASE_URL:
+#     raise Exception("No DATABASE_URL set for production!")
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=DATABASE_URL,
+#         conn_max_age=600,
+#         ssl_require=True,
+#     )
+# }
 
 
 # Password validation
